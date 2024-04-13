e52426db576f7f3258686a43d788199c01f1b4b6
Esmelyn04 <EML_JOSE_BAEZ@HOTMAIL.COM>
2024-04-12 23:59:42 -0400

    Get Submissions with Client Side Routing

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -15,10 +15,12 @@ export async function action() {
 
 }
 
-export async function loader() {
-    const contacts = await getContacts();
-    return { contacts };
-  }
+export async function loader({ request }) {
+  const url = new URL(request.url);
+  const q = url.searchParams.get("q");
+  const contacts = await getContacts(q);
+  return { contacts };
+}
 
 export default function Root() {
     const { contacts } = useLoaderData();
@@ -29,7 +31,7 @@ export default function Root() {
         <div id="sidebar">
           <h1>React Router Contacts</h1>
           <div>
-            <form id="search-form" role="search">
+            <Form id="search-form" role="search">
               <input
                 id="q"
                 aria-label="Search contacts"
@@ -46,7 +48,7 @@ export default function Root() {
                 className="sr-only"
                 aria-live="polite"
               ></div>
-            </form>
+            </Form>
             <Form method="post">
               <button type="submit">New</button>
             </Form>
